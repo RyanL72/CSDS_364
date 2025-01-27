27ae44469ccb99f650931d753d12455f34eeb09e
Ryan_Lin2 <rhl72@case.edu>
2025-01-27 17:28:03 -0500

    Honest Working Gabor

--- Assignment_A1b/gabor.py
+++ Assignment_A1b/gabor.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Assignment_A1b.trigwaves import *
+from trig import *
 
 def gaussian_envelope(t, sigma):
     return np.e ** (- ((t**2)  / (2 * sigma**2)))
@@ -8,31 +8,85 @@ def gabor(t, sigma, f, phi=0, a=1):
     return a * gaussian_envelope(t, sigma) * coswave_phi(t, f, phi)
 
 def gaboro(t, sigma, f, phi=0, a=1):
-    return gabor(t, sigma, f, phi, a)
-
-def gabore(t, sigma, f, phi, a):
     return a * gaussian_envelope(t, sigma) * sinwave_phi(t, f, phi)
 
-def gabor_norm(t, sigma, f, phi=0, a=1):
-    sum = 0
-    for value in  t:
-        g = gabor(value, sigma, f, phi, a)
-        g_squared = g**2
-        sum+=g_squared
-    return 1/sum
+def gabore(t, sigma, f, phi=0, a=1):
+    return gabor(t , sigma, f, phi, a)
 
-def gabore_norm(t, sigma, f, phi=0, a=1):
-    sum = 0
-    for value in  t:
-        g = gabore(value, sigma, f, phi, a)
-        g_squared = g**2
-        sum+=g_squared
-    return 1/sum
+def gabor_norm(f, sigma, fs, d=0):
+    """
+    Calculate the normalizing constant 
+    """
 
-def gaboro_norm(t, sigma, f, phi=0, a=1):
-    sum = 0
-    for value in  t:
-        g = gaboro(value, sigma, f, phi, a)
-        g_squared = g**2
-        sum+=g_squared
-    return 1/sum
+    # Calculate t vector
+    t_range = 3 * sigma 
+    num_samples = int(2 * t_range * fs)
+    t = np.linspace(-t_range, t_range, num_samples)
+
+    #Get g(t) values
+    g_values = gaussian_envelope(t, sigma) * coswave(t, f, d)
+    energy = np.trapz(g_values**2, t)
+
+    return 1 / np.sqrt(energy)
+
+def gabore_norm(f, sigma, fs, d=0):
+    """
+    Calculate the normalizing constant 
+    """
+
+    # Calculate t vector
+    t_range = 3 * sigma 
+    num_samples = int(2 * t_range * fs)
+    t = np.linspace(-t_range, t_range, num_samples)
+
+    #Get g(t) values
+    # Custom Sine function will be equivelent to cos if phi = pi/4
+    g_values = gaussian_envelope(t, sigma) * coswave(t, f, d)
+    energy = np.trapz(g_values**2, t)
+
+    return 1 / (np.sqrt(energy))
+
+def gaboro_norm(f, sigma, fs, d=0):
+    """
+    Calculate the normalizing constant 
+    """
+
+    # Calculate t vector
+    t_range = 3 * sigma 
+    num_samples = int(2 * t_range * fs)
+    t = np.linspace(-t_range, t_range, num_samples)
+
+    #Get g(t) values
+    # Custom Sine function will be equivelent to cos if phi = pi/4
+    g_values = gaussian_envelope(t, sigma) * sinewave(t, f, d)
+    energy = np.trapz(g_values**2, t)
+
+    return 1 / np.sqrt(energy)
+
+
+
+# Initial Implementations, not using samping frequency
+
+# def gabor_norm(t, sigma, f, phi=0, a=1):
+#     sum = 0
+#     for value in  t:
+#         g = gabor(value, sigma, f, phi, a)
+#         g_squared = g**2
+#         sum+=g_squared
+#     return 1/sum
+
+# def gabore_norm(t, sigma, f, phi=0, a=1):
+#     sum = 0
+#     for value in  t:
+#         g = gabore(value, sigma, f, phi, a)
+#         g_squared = g**2
+#         sum+=g_squared
+#     return 1/sum
+
+# def gaboro_norm(t, sigma, f, phi=0, a=1):
+#     sum = 0
+#     for value in  t:
+#         g = gaboro(value, sigma, f, phi, a)
+#         g_squared = g**2
+#         sum+=g_squared
+#     return 1/sum
